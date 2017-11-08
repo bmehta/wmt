51a2040b9c6a794f967377a3588457d66232a7cd
Binita Mehta <binita_mehta@yahoo.com>
2017-11-08 16:22:18 -0500

    Added styling for search page.

--- app/app.js
+++ app/app.js
@@ -5,7 +5,9 @@ TODO
 - unit tests
 - better UI using bootstrap
 - e2e tests
-- CORS error for recommendations api
+- CORS error for recommendations api (document)
+- product lookup and throttling for recommendations
+- handle no recommendations errors - nbp?apiKey=d9nr24chkrrd9p4sgck8v5ub&itemId=14871803
  */
 
 // Declare app level module which depends on views, and components
