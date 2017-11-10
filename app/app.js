981c480adc9f3f431833120a4bac4ec869e217ba
Binita Mehta <binita_mehta@yahoo.com>
2017-11-10 00:27:13 -0500

    TODOs removed.

--- app/app.js
+++ app/app.js
@@ -2,19 +2,15 @@
 
 /*
 TODO
-- unit tests
-*- better UI using bootstrap
-- e2e tests
+
 - README.md
 - CORS error for recommendations api (document)
  - Move out CSS styles to app.css
- - console errors for html decode/escape
 - product lookup and throttling for recommendations
 - comments
-*- handle no recommendations errors - nbp?apiKey=d9nr24chkrrd9p4sgck8v5ub&itemId=14871803
+- utility service
 - footer
-*- add more details to details page
-*- limit to 10 recommendations
+- read api documentation
  */
 
 // Declare app level module which depends on views, and components
